dd32ad5148ccf1b26c2f523a48e45defec054b52
Edi Kusyanto <edikusyanto08@gmail.com>
2023-07-01 10:33:35 +0700

    Update TanggalFormat.php dd mmmm yyyy

--- src/TanggalFormat.php
+++ src/TanggalFormat.php
@@ -21,4 +21,13 @@ class TanggalFormat
 
         return $date->format(($format != null ? $format : 'l, j F Y')); // Selasa, 16 Maret 2021 ; 08:27 pagi
     }
+    public static function TanggalIndo(string $date, string $format = null)
+    {
+        $locale = config('laraindo', true);
+        $date = Carbon::parse($date)->locale($locale['TanggalFormat']['locale']);
+
+        $date->settings(['formatFunction' => 'translatedFormat']);
+
+        return $date->format(($format != null ? $format : 'j F Y')); // 16 Maret 2021
+    }
 }
